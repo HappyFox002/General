c64a74f4892a185249715bf37fd4e56d72568ae0
sashazvereva <sasazvereva15@fmail.com>
2020-01-21 17:33:46 +0300

    Create Program.cs

---
+++ for20/for20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace for20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =  1;
+            float sum = 0;
+            n = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i <= n; i++)
+            {
+                sum = n * i;
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
