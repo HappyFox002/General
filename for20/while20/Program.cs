943477d4a5d46e94ad0aefb2e7e1358bb3cc8f7b
sashazvereva <sasazvereva15@fmail.com>
2020-01-21 17:58:18 +0300

    while 20

---
+++ for20/while20/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace while20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int k = 0;
+            float i = 0, s = 0, n = 0; 
+            n = Convert.ToInt32(Console.ReadLine());
+            while (s <= n) 
+            {
+                k++;
+                if (s % n = 2)
+                {
+                    Console.WriteLine("TRUE");
+                        else Console.WriteLine("FALSE");
+                }
+            }
+        }
+    }
+}
